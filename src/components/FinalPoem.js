09cc277f27a9754a5f0b0f9f2f01326a2475d4ec
Jessica Stone <jstone4815@gmail.com>
2020-04-21 19:00:15 -0700

    loop through submissions and add to an array

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,11 +3,21 @@ import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
+  // const completedPoem = props.submissions.map(submission => submission.);
+  const sentences = [];
+  
+  props.submissions.forEach((submission) => {    
+    const sentence = "The " + submission.adjective1 + " " + submission.noun1 + " " + submission.adverb + " " + submission.verb + " the " + submission.adjective2 + " " + submission.noun2 + ".";
+    sentences.push(sentence)
+  });
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
 
+        {/* As players who have submitted one or more lines, I want to see all of the submissions of poetry lines in the section named "Final Poem".
+        As a player, I want to see each submission in the final poem section on a different line or paragraph, so that it looks more like a structured poem. */}
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
