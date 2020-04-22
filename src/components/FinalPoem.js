edc29f42047f0a401e38b011dd6852110b9e0689
Jessica Stone <jstone4815@gmail.com>
2020-04-21 19:36:21 -0700

    add logic to show final poem with each sentence on new line when button is clicked

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
-  // const completedPoem = props.submissions.map(submission => submission.);
-  const sentences = [];
-  
-  props.submissions.forEach((submission) => {    
-    const sentence = "The " + submission.adjective1 + " " + submission.noun1 + " " + submission.adverb + " " + submission.verb + " the " + submission.adjective2 + " " + submission.noun2 + ".";
-    sentences.push(sentence)
+  const [display, setDisplay] = useState(false);
+
+  const sentences = props.submissions.map((submission) => {    
+    return "The " + submission.adjective1 + " " + submission.noun1 + " " + submission.adverb + " " + submission.verb + " the " + submission.adjective2 + " " + submission.noun2 + ".";
   });
 
   let finalPoem = sentences.map(sentence => {
@@ -17,18 +15,24 @@ const FinalPoem = (props) => {
     ) 
   });
 
+  const onReveal = () => {
+    setDisplay(true);
+  }
+
+
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
-        {/* As players who have submitted one or more lines, I want to see all of the submissions of poetry lines in the section named "Final Poem".
-        As a player, I want to see each submission in the final poem section on a different line or paragraph, so that it looks more like a structured poem. */}
-        {finalPoem}
-      </section>
+      {/* if display is true, show final poem */}
+      { display ? 
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+          {finalPoem}
+        </section> : 
+        null
+      }
 
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={onReveal}/>
       </div>
     </div>
   );
