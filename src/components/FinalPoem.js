550343a7452c1a3d4dd85be8819cafe0f3e7f306
Jessica Stone <jstone4815@gmail.com>
2020-04-22 17:47:45 -0700

    remove display state and add callback function to change submitted state in Game

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,20 +3,20 @@ import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
-  const [display, setDisplay] = useState(false);
+  // const [display, setDisplay] = useState(false);
 
   let finalPoem = props.submissions.map(sentence => {
     return (<p>{sentence}</p>) 
   });
 
   const onReveal = () => {
-    setDisplay(true);
+    props.onFinalPoemCallback(true);
   }
 
   return (
     <div className="FinalPoem">
       {/* if display is true, show final poem */}
-      { display ? 
+      { props.submitted ? 
         <section className="FinalPoem__poem">
           <h3>Final Poem</h3>
           {finalPoem}
@@ -24,9 +24,12 @@ const FinalPoem = (props) => {
         null
       }
 
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={onReveal}/>
-      </div>
+      { props.submitted ? 
+        null : 
+        <div className="FinalPoem__reveal-btn-container">
+          <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={onReveal}/>
+        </div>
+      }
     </div>
   );
 }
