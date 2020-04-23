d23992319ac570f2df6f76936d7254629a09acf7
Jessica Stone <jstone4815@gmail.com>
2020-04-22 18:01:24 -0700

    add input validation for blank fields

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -41,68 +41,73 @@ const PlayerSubmissionForm = (props) => {
     }
   }
 
+  return (
+  <div className="PlayerSubmissionForm">
+    <h3>Player Submission Form for Player #{ props.player }</h3>
 
-    return (
-    <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{ props.player }</h3>
+    <form className="PlayerSubmissionForm__form" onSubmit={onSubmitLine}>
 
-      <form className="PlayerSubmissionForm__form" onSubmit={onSubmitLine}>
+      <div className="PlayerSubmissionForm__poem-inputs">
 
-        <div className="PlayerSubmissionForm__poem-inputs">
+        <p>The</p>
+        <input
+          name="adjective1"
+          className={(sentence.adjective1 === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="adjective"
+          type="adjective" 
+          value={sentence.adjective1} 
+          onChange={onInputChange}
+        />
+        <input
+          name="noun1"
+          className={(sentence.noun1 === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="noun"
+          type="noun" 
+          value={sentence.noun1} 
+          onChange={onInputChange}
+        />
+        <input
+          name="adverb"
+          className={(sentence.adverb === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="adverb"
+          type="adverb" 
+          value={sentence.adverb} 
+          onChange={onInputChange}
+        />
+        <input
+          name="verb"
+          className={(sentence.verb === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="verb"
+          type="verb" 
+          value={sentence.verb} 
+          onChange={onInputChange}
+        />
+        <p>the</p>
+        <input
+          name="adjective2"
+          className={(sentence.adjective2 === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="another adjective"
+          type="adjective" 
+          value={sentence.adjective2} 
+          onChange={onInputChange}
+        />
+        <input
+          name="noun2"
+          className={(sentence.noun2 === "") ? "PlayerSubmissionFormt__input--invalid" : ""} 
+          placeholder="another noun"
+          type="noun" 
+          value={sentence.noun2} 
+          onChange={onInputChange}
+        />
+        <p>.</p>
 
-          <p>The</p>
-          <input
-            name="adjective1"
-            placeholder="adjective"
-            type="adjective" 
-            value={sentence.adjective1} 
-            onChange={onInputChange}
-          />
-          <input
-            name="noun1"
-            placeholder="noun"
-            type="noun" 
-            value={sentence.noun1} 
-            onChange={onInputChange}
-          />
-          <input
-            name="adverb"
-            placeholder="adverb"
-            type="adverb" 
-            value={sentence.adverb} 
-            onChange={onInputChange}
-          />
-          <input
-            name="verb"
-            placeholder="verb"
-            type="verb" 
-            value={sentence.verb} 
-            onChange={onInputChange}
-          />
-          <p>the</p>
-          <input
-            name="adjective2"
-            placeholder="another adjective"
-            type="adjective" 
-            value={sentence.adjective2} 
-            onChange={onInputChange}
-          />
-          <input
-            name="noun2"
-            placeholder="another noun"
-            type="noun" 
-            value={sentence.noun2} 
-            onChange={onInputChange}
-          />
-          <p>.</p>
+      </div>
 
-        </div>
-
-        <div className="PlayerSubmissionForm__submit">
-          <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
-        </div>
-      </form>
-    </div>
+      <div className="PlayerSubmissionForm__submit">
+        <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+      </div>
+    </form>
+  </div>
   );
 }
 
